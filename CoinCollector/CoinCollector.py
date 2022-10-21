7156b72400acfd71844dfe7138f05d1fcc583587
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-10-22 00:01:19 +0900

    Coin Collector

--- CoinCollector/CoinCollector.py
+++ CoinCollector/CoinCollector.py
@@ -1,8 +1,8 @@
-#How to start?: cmd - pgzrun CoinCollector.py
+import pgzrun
 from random import randint
 
-WIDTH = 400 
-HEIGHT = 400 
+WIDTH = 400
+HEIGHT = 400
 
 score = 0
 
@@ -11,65 +11,43 @@ game_over = False
 fox = Actor("fox")
 fox.pos = 100, 100
 
-coin = Actor("coin") 
+coin = Actor("coin")
 coin.pos = 200, 200
 
- 
 def draw():
-	screen.fill("lightgreen") 
-	fox.draw() 
-	coin.draw()
-	screen.draw.text("Score: " + str(score), color="black", topleft=(10,10))
-
-
-	# Game Over Screeb
-	if game_over:
-		screen.fill("pink") # Change background to pink.
-		screen.draw.text("Times Up! :( ", topleft=(10,60), fontsize=60) 
-		screen.draw.text("Final Score: " + str(score), color="black", topleft=(10,10), fontsize=60) 
-
-
-
-
+    screen.fill("green")
+    fox.draw()
+    coin.draw()
+    screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
+    
+    if game_over:
+        screen.fill("pink")
+        screen.draw.text("Final Score: " + str(score), topleft=(10, 10), fontsize=60)
 def place_coin():
-	coin.x = randint(20, (WIDTH - 20))
-	coin.y = randint(20, (HEIGHT - 20))
+    coin.x = randint(20, (WIDTH - 20))
+    coin.y = randint(20, (HEIGHT -20))
 
 def time_up():
-	global game_over
-	game_over = True
-	place_coin()
-
-
+    global game_over
+    game_over = True
 
 def update():
-	global score
+    global score
+    
+    if keyboard.left:
+        fox.x = fox.x - 2
+    elif keyboard.right:
+        fox.x = fox.x + 2
+    elif keyboard.up:
+        fox.y = fox.y - 2
+    elif keyboard.down:
+        fox.y = fox.y + 2
+        
+    coin_collected = fox.colliderect(coin)
+    
+    if coin_collected:
+        score = score + 10
+        place_coin()
 
-	speed = 5
-	if keyboard.left:
-		fox.x = fox.x - speed
-		fox.image = "fox"
-
-	if keyboard.right:
-		fox.x = fox.x + speed
-		fox.image = "fox"
-
-
-	if keyboard.up:
-		fox.y = fox.y - speed
-
-	if keyboard.down:
-		fox.y = fox.y + speed
-
-	coin_collected = fox.colliderect(coin)
-
-	if coin_collected:
-		score = score + 10
-		place_coin()
-		speed = speed - .2
-
-
-
-clock.schedule(time_up, 16.0)
+clock.schedule(time_up, 10.0)
 place_coin()
-
