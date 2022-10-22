cf81060a9b18f80385913b834eb8a9e0190a7d32
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-10-23 02:10:33 +0900

    modify code

--- DrawRhombus.py
+++ DrawRhombus.py
@@ -1,23 +1,24 @@
 inputValue = int(input('input interger:'))
 
-base = ' '
-for i in range(inputValue):
-    base = ' '+str(2*(inputValue-1-i)+1)+base+str(2*(inputValue-1-i)+1)+' '
-
-length = len(base.strip())
+length =0
 graph = []
 for j in range(inputValue):
     line = ' '
+
+    #공백 넣기
     for k in range(j):
         line = '  '+line+'  ' + '   '
+
+    #문자열 등록
     for i in range(inputValue-j):
         line = ' '+str(2*(inputValue-1-i)+1)+line+str(2*(inputValue-1-i)+1)+' '
+        
     line = line.strip()
-    length_temp = int((length-len(line))/2)
-    for m in range(length_temp):
-        line = ' '+line+' '
     graph.append(line)
+
+#반대로 한번 더 출력
 for j in range(inputValue):
     graph.append(graph[inputValue-1-j])
+    
 for line in graph:
     print(line)
